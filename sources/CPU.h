406b5762449dedef9cd5f49b39f4fee61b54df83
Chiro_11 <1750756@tongji.edu.cn>
2019-05-21 22:54:20 +0800

    First Commit

---
+++ sources/CPU.h
@@ -0,0 +1,37 @@
+using namespace std;
+
+class CPU {
+    int a, b, c, d, e, h, l, f;
+    int sp, pc;
+
+    void init();
+    void LDrr_bb();
+    void LDrr_bc();
+    void LDrr_bd();
+    void LDrr_be();
+    void LDrr_bh();
+    void LDrr_bl();
+    void LDrr_ba();
+    void LDrr_cb();
+    void LDrr_cc();
+    void LDrr_cd();
+    void LDrr_ce();
+    void LDrr_ch();
+    void LDrr_cl();
+    void LDrr_ca();
+    void LDrr_db();
+    void LDrr_dc();
+    void LDrr_dd();
+    void LDrr_de();
+    void LDrr_dh();
+    void LDrr_dl();
+    void LDrr_da();
+    void LDrr_eb();
+    void LDrr_ec();
+    void LDrr_ed();
+    void LDrr_ee();
+    void LDrr_eh();
+    void LDrr_el();
+    void LDrr_ea();
+    void LDrr_hb();
+};
